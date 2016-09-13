7c0d1330bfb105b2b1f368c758c6f0909aa0df65
Ajay Dinakar Kandavalli <ajaydinakar6@gmail.com>
2016-09-13 10:15:19 -0500

    Update myrandom.sh

--- assignments/homework-01/myrandom.sh
+++ assignments/homework-01/myrandom.sh
@@ -1,16 +1,13 @@
-
-
 #!/bin/bash
 file=/usr/share/dict/words     #Assigning the complete path of the file "words" to variable "FILE"
 if [ -e $file ]                #Checking if the given file exists
 then
 wordnum=$(cat $file | wc -l)       #Counting number of lines present in the file and displaying
-#Selecting a randoom number brtween  1 to number of lines
+#Selecting a random number between  1 to number of lines
 range=$((wordnum+1))
-R=$(($RANDOM%$range))
-
-#Extracting the word present in that line and displaying it
-word=$(head -$R $file | tail -1)
+rand=$(($RANDOM%$range))
+#Extracting the word present in that line and assigning it to word and displaying it
+word=$(head -$rand $file | tail -1)
 echo "RANDOM WORD:  "$word
 else 
 echo "file doesnot exist"
