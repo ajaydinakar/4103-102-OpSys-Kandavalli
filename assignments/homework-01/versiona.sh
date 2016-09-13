6c7f81a402c3946230f4af3cbbdc6fcfcf361b52
Ajay Dinakar Kandavalli <ajaydinakar6@gmail.com>
2016-09-13 10:23:17 -0500

    Update versiona.sh

--- assignments/homework-01/versiona.sh
+++ assignments/homework-01/versiona.sh
@@ -6,14 +6,12 @@ then
 dt=$(date +"%Y-%m-%d")                 #Taking present date into a variable using command line substitution
 cp $file "$dt"_"$file"                 #copying the contents of input file into a new file
 echo $dt_$1
-
 #If the input file doesnot exist
 else
-echo "file doesnot exist created a file and  dated copy"
-
-
+echo "file doesnot exist created a file and it's dated copy"
 #Creating a new empty file and creating a new dated copy file
 dt=$(date +"%Y-%m-%d")
 touch "$file"
 cp $file "$dt"_"$file"
+echo "dated copy of the file created"
 fi
