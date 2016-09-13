83d906cc8f27da8277768eb63dd4e2003badc624
Ajay Dinakar Kandavalli <ajaydinakar6@gmail.com>
2016-09-13 10:19:34 -0500

    Update versiona.sh

--- assignments/homework-01/versiona.sh
+++ assignments/homework-01/versiona.sh
@@ -5,7 +5,7 @@ if [ -e $file ]                         #Checking if given file exists
 then
 dt=$(date +"%Y-%m-%d")                 #Taking present date into a variable using command line substitution
 cp $file "$dt"_"$file"                 #copying the contents of input file into a new file
-echo $DATE_$1
+echo $dt_$1
 
 #If the input file doesnot exist
 else
