848ff207631567892947eb5c1e8815ac81a76c4a
Ajay Dinakar Kandavalli <ajaydinakar6@gmail.com>
2016-09-13 10:27:30 -0500

    Update versionb.sh

--- assignments/homework-01/versionb.sh
+++ assignments/homework-01/versionb.sh
@@ -5,17 +5,16 @@ if [ -e $file ]                              #Checking if the input file exists
 then
 dt=$(date +"%Y-%m-%d")                      #Assigning present date into a variable
 e=$(echo ${file}|awk -F\. '{print $2}')      #Storing the extension of the file into a variable
-f=$(echo ${file%*.*}_"$dt"."$e")            #Creating a new file as filename followed by date and extension of the file
-cp $file $f                                  #Copying the contents of input file to new file
-
+datedfile=$(echo ${file%*.*}_"$dt"."$e")            #Creating a new file as filename followed by date and extension of the file
+cp $file $datedfile                                  #Copying the contents of input file to new file
+echo "dated copy of the file has been created"
 #if the file does nto exist
 else
-echo "file doesnot exist so creating a new file and dated copy"
-
+echo "file doesnot exist so creating a new file and it's dated copy"
 #Creating a new empty file and creating a dated copy
 touch $file
 dt=$(date +"%Y-%m-%d")
 e=$(echo ${file}|awk -F\. '{print $2}')
-f=$(echo ${file%*.*}_"$dt"."$e")
-cp $file $f
+datedfile=$(echo ${file%*.*}_"$dt"."$e")
+cp $file $datedfile
 fi
