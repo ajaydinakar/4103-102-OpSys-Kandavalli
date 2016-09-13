c38424b97ab883880f80b74be72c65563efb5991
ajaydinakar <ajaydinakar6@gmail.com>
2016-09-13 11:00:05 -0400

    edited file

--- assignments/homework-01/myrandom.sh
+++ assignments/homework-01/myrandom.sh
@@ -1,16 +1,16 @@
 
 
 #!/bin/bash
-FILE=/usr/share/dict/words     #Assigning the complete path of the file "words" to variable "FILE"
-if [ -e $FILE ]                #Checking if the given file exists
+file=/usr/share/dict/words     #Assigning the complete path of the file "words" to variable "FILE"
+if [ -e $file ]                #Checking if the given file exists
 then
-wordnum=$(cat $FILE | wc -l)       #Counting number of lines present in the file and displaying
+wordnum=$(cat $file | wc -l)       #Counting number of lines present in the file and displaying
 #Selecting a randoom number brtween  1 to number of lines
 DIV=$((wordnum+1))
 R=$(($RANDOM%$DIV))
 
 #Extracting the word present in that line and displaying it
-T=$(head -$R $FILE | tail -1)
+T=$(head -$R $file | tail -1)
 echo "RANDOM WORD:  "$T
 else 
 echo "file doesnot exist"
--- assignments/homework-01/versionb.sh
+++ assignments/homework-01/versionb.sh
@@ -3,9 +3,9 @@
 file=$1
 if [ -e $file ]                              #Checking if the input file exists
 then
-now=$(date +"%Y-%m-%d")                      #Assigning present date into a variable
+dt=$(date +"%Y-%m-%d")                      #Assigning present date into a variable
 e=$(echo ${file}|awk -F\. '{print $2}')      #Storing the extension of the file into a variable
-f=$(echo ${file%*.*}_"$now"."$e")            #Creating a new file as filename followed by date and extension of the file
+f=$(echo ${file%*.*}_"$dt"."$e")            #Creating a new file as filename followed by date and extension of the file
 cp $file $f                                  #Copying the contents of input file to new file
 
 #if the file does nto exist
@@ -14,8 +14,8 @@ echo "file doesnot exist so creating a new file and dated copy"
 
 #Creating a new empty file and creating a dated copy
 touch $file
-now=$(date +"%Y-%m-%d")
+dt=$(date +"%Y-%m-%d")
 e=$(echo ${file}|awk -F\. '{print $2}')
-f=$(echo ${file%*.*}_"$now"."$e")
+f=$(echo ${file%*.*}_"$dt"."$e")
 cp $file $f
 fi
