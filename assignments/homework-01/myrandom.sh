90c5322d2c540bcb224f2bb1f89572af91b40422
Ajay Dinakar Kandavalli <ajaydinakar6@gmail.com>
2016-09-13 10:03:54 -0500

    Update myrandom.sh

--- assignments/homework-01/myrandom.sh
+++ assignments/homework-01/myrandom.sh
@@ -6,12 +6,12 @@ if [ -e $file ]                #Checking if the given file exists
 then
 wordnum=$(cat $file | wc -l)       #Counting number of lines present in the file and displaying
 #Selecting a randoom number brtween  1 to number of lines
-DIV=$((wordnum+1))
-R=$(($RANDOM%$DIV))
+range=$((wordnum+1))
+R=$(($RANDOM%$range))
 
 #Extracting the word present in that line and displaying it
-T=$(head -$R $file | tail -1)
-echo "RANDOM WORD:  "$T
+word=$(head -$R $file | tail -1)
+echo "RANDOM WORD:  "$word
 else 
 echo "file doesnot exist"
 fi
